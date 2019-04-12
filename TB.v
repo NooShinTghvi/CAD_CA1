827a1732a73deea739eff9c9d2292ad3e47eca85
NooShin Taghavi <nooshin.tghvi@gmail.com>
2019-04-12 13:45:12 +0430

    added load to input of modules

--- TB.v
+++ TB.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ns
 
 module A;
-	reg clk,rst;
+	reg clk,rst,ld;
     reg [7:0] a,b;
     wire [7:0] sum;
     wire cout;
-	serial_adder #(8) U( a,b,clk,rst,sum,cout);
+	serial_adder #(8) U( a,b,rst,ld,clk,sum,cout);
 	
 	initial begin
 		clk = 1'b0;
 		rst = 1'b1;
+		ld  = 1'b0; 
         a   = 8'b11101010;
         b   = 8'b11110011;
 	end
@@ -17,7 +18,10 @@ module A;
 	initial begin
 		#20
 		rst = 1'b0;
+		ld  = 1'b1;
 		#160
 		$stop();
+		// sum = 11011101
+		//cout = 1
 	end
 endmodule
--- adder.v
+++ adder.v
@@ -1,20 +1,20 @@
 module serial_adder #(parameter N = 4) (
         input [N-1:0] a,b,
-        input clk,rst,
+        input rst,ld,clk,
         output [N-1:0] sum,
         output cout
     );
     wire carry_in,carry_out,a_out,b_out,sum_in;
-    shift_register_out #(N) c1 (clk,rst,a,a_cout);
-    shift_register_out #(N) c2 (clk,rst,b,b_cout);
+    shift_register_out #(N) c1 (clk,rst,ld,a,a_cout);
+    shift_register_out #(N) c2 (clk,rst,ld,b,b_cout);
     assign {carry_out,sum_in} = a_cout + b_cout + carry_in;
-    Dff c3(clk,rst,carry_out,carry_in);
-    shift_register_in #(N) c4 (clk,rst,sum_in,sum);
+    Dff c3(clk,rst,ld,carry_out,carry_in);
+    shift_register_in #(N) c4 (clk,rst,ld,sum_in,sum);
     assign cout = carry_out;
 endmodule
 
 module shift_register_out #(parameter N = 4) (
-        input clk,rst,
+        input clk,rst,ld,
         input [N-1:0] q,
         output y_out
     );
@@ -31,7 +31,7 @@ module shift_register_out #(parameter N = 4) (
 endmodule
 
 module shift_register_in #(parameter N = 4) (
-        input clk,rst,y_in,
+        input clk,rst,ld,y_in,
         output [N-1:0] y
     );
     wire [N-1:0] temp;
@@ -44,7 +44,7 @@ module shift_register_in #(parameter N = 4) (
 endmodule
 
 module Dff (
-    input clk,rst,in_dff,
+    input clk,rst,ld,in_dff,
     output out_dff
 );
     wire out_next;
